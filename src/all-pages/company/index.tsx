26ea8f4c61061d54060edac15cbddde9575a3df0
rezkaudi <audirezk@gmail.com>
2025-02-13 21:01:01 +0300

    build stracture for all pages

---
+++ src/all-pages/company/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+// sections
+import Hero from "./sections/Hero";
+import OtherContents from "./sections/OtherContents";
+import Contact from "./sections/Contact";
+
+
+const Index = () => {
+    return (
+        <>
+            <Hero />
+            <OtherContents />
+            <Contact />
+        </>
+    )
+};
+
+export default Index;
