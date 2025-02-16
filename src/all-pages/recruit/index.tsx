ff94a67df6615f199f3d6d8f91bf299828bfbc16
rezkaudi <audirezk@gmail.com>
2025-02-16 14:27:37 +0300

    end company and recruit page

--- src/all-pages/recruit/index.tsx
+++ src/all-pages/recruit/index.tsx
@@ -4,12 +4,16 @@ import React from "react";
 import Hero from "./sections/Hero";
 import OtherContents from "./sections/OtherContents";
 import Contact from "./sections/Contact";
+import Section2 from "./sections/Section2";
+import Section3 from "./sections/Section3";
 
 
 const Index = () => {
     return (
         <>
             <Hero />
+            <Section2 />
+            <Section3 />
             <OtherContents />
             <Contact />
         </>
