d633d063de62af8202091ccd5345a12555a893ec
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2025-03-21 03:35:12 +0900

    show all the categories

--- src/app/api/articles/route.ts
+++ src/app/api/articles/route.ts
@@ -3,9 +3,9 @@ import axios from 'axios';
 
 export async function GET() {
   try {
-    const response = await axios.get('http://82.165.187.125:1337/api/articles?filters[category][id][$eq]=6&sort[0]=id:desc', {
+    const response = await axios.get('http://82.165.187.125:1337/api/articles?populate=category&sort[0]=id:desc', {
       headers: {
-        Authorization: `Bearer ed3a20bc94f6ed30ca1349481b63f438a51ce231d09a56a21577edf10ff8b8b342af2d43521dda9c760afd638698368c621b3d2839a9630d20ae9f6c1318c2bd9580cfc6097e4afd37583c715328b9041102a8118995c5ce10ba7015406604272f89bfc1fc09763c636777d325cb9a9d7f840133fbc6e597cfdccd689220d3ec`, // Replace with your actual token
+        Authorization: `Bearer ed3a20bc94f6ed30ca1349481b63f438a51ce231d09a56a21577edf10ff8b8b342af2d43521dda9c760afd638698368c621b3d2839a9630d20ae9f6c1318c2bd9580cfc6097e4afd37583c715328b9041102a8118995c5ce10ba7015406604272f89bfc1fc09763c636777d325cb9a9d7f840133fbc6e597cfdccd689220d3ec`,
         "X-TENANT-ID": "28",
       },
     });
