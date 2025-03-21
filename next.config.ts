0f17c90918f12c8a922fcc42cc4b18007e90eb66
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2025-03-21 11:15:03 +0900

    securing api token

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    STRAPI_API_TOKEN: process.env.STRAPI_API_TOKEN,
+    TENANT_ID: process.env.TENANT_ID,
+  },
 };
 
 export default nextConfig;
