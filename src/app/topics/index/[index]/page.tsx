5d2ee6b0d774eaf11ca5e09daefb057d571fe695
rezkaudi <audirezk@gmail.com>
2025-04-27 14:45:46 +0300

    solve blog images issue

--- src/app/topics/index/[index]/page.tsx
+++ src/app/topics/index/[index]/page.tsx
@@ -113,7 +113,12 @@ const ArticleDetailsByIndex: React.FC = () => {
 
           <div
             className="article-content"
-            dangerouslySetInnerHTML={{ __html: article.Body }}
+            dangerouslySetInnerHTML={{
+              __html: article.Body.replaceAll(
+                'http://82.165.187.125:1337/',
+                '/api/proxy?url=82.165.187.125:1337/'
+              )
+            }}
           />
         </div>
       </div>
